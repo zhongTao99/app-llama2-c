1b7ff2671218da450185e174335a50e177f836cf
Razvan Deaconescu <razvand@unikraft.io>
2023-09-28 17:35:20 +0300

    Replace Makefile with setup script
    Use setup script instead of Makefile for setting up the environment.
    Use `workdir/` instead of `UNIK/` as directory name storing repository
    clones.
    
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MOD_PATH=out/model.bin
+TOK_PATH=tokenizer.bin
+
+[ ! -d "out/" ] && echo "Downloading model..." || true
+[ ! -d "out/" ] && wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories15M.bin || true
+[ ! -d "out/" ] && mkdir out || true
+[ -f "stories15M.bin" ] && mv stories15M.bin out/model.bin || true
+[ ! -d "workdir" ] && echo "Cloning unikraft and musl sources..." || true
+[ ! -d "workdir/unikraft" ] && git clone https://github.com/unikraft/unikraft workdir/unikraft || true
+[ ! -d "workdir/libs/musl" ] && git clone https://github.com/unikraft/lib-musl workdir/libs/musl || true
+gcc -Ofast strliteral.c -o strlit
+./strlit -i emb_Model_data $MOD_PATH model.h
+./strlit -i emb_Tokenizer_data $TOK_PATH tokenizer.h
